dd17326e6b81a8c7ad8a692a266784d131a07f12
nero <pask4857@gmail.com>
2025-12-19 16:14:21 +0800

    重做1219

--- src/main/java/com/example/backend/category/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.category.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/example/backend/product/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.product.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
